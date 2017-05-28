8115d457ff365182fcce5cb7d62d02b78c2e18be
telwell <me@trevorelwell.com>
2017-05-28 13:43:20 -0400

    adding question 5 solution

--- set_1/spec/1_spec.rb
+++ set_1/spec/1_spec.rb
@@ -47,4 +47,15 @@ RSpec.describe Crypto, "set 1" do
   end
   
   
+  context "question 5" do
+    it "should be able to implement a repeating-key XOR" do
+      plain_text = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+      key = 'ICE'
+      expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+      cipher_text = Converter.str_to_hex(Crypto.xor_key(plain_text, key))
+      expect(cipher_text).to eq(expected)
+    end
+  end
+  
+  
 end
